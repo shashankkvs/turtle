432a87f8766ef618793d40ce6966e5838e03a6b9
shashankkvs <shashank.kvs0308@gmail.com>
2021-02-02 23:03:44 +0530

    first commit

---
+++ tfunc.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+def arrow_c(size=None):
+    global sc
+    sc=Screen()
+    sc.bgcolor("black")
+    
+if __name__ == "__main__":
+        pointer = Turtle()
+        pointer.color(input("enter color:"))
+        arrow_c()
+        while True:
+           pass 
+
